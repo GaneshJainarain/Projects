fb118787748e641ac979d577a4d2b30db6d53a69
Ganesh Jainarain <47873288+RicheyJay23@users.noreply.github.com>
2020-04-27 21:28:46 -0400

    Add files via upload

---
+++ fib.py
@@ -0,0 +1,16 @@
+# fib.py
+def loopFib(n):
+    # pre: n > 0
+    # returns the nth Fibonacci number
+
+    curr = 1
+    prev = 1
+    for i in range(n-2):
+        curr, prev = curr+prev, curr
+    return curr
+
+def recFib(n):
+    if n < 3:
+        return 1
+    else:
+        return recFib(n-1) + recFib(n-2)
